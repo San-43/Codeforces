a78cb287d8bbaf9d33ac50c816d7345000bfbc74
Sandro Martínez <virgosmp.mx@gmail.com>
2025-04-09 00:05:34 -0600

    L

--- Code Rush 2025/L/main.cpp
+++ Code Rush 2025/L/main.cpp
@@ -1,25 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+void solve() {
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> a(n);
+    vector<int> b(m);
+    vector<int> c(k);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> b[i];
+    }
+    for (int i = 0; i < k; i++) {
+        cin >> c[i];
     }
 
-    return 0;
+
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+}
